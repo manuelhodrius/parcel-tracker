855016f4ff018a5995f3179798b621000b978b04
Manuel Hodrius <manuel@hodrius.com>
2018-01-22 21:41:24 -0800

    changes in #startup

--- #startup.py
+++ #startup.py
@@ -2,7 +2,7 @@ import subprocess
 from time import sleep
 
 y=(0.1)
-subprocess.Popen(["python3", '#logger.py'])
+subprocess.Popen(["python3", 'parcel-tracker/#logger.py'])
 sleep(y)
-subprocess.Popen(["python", '#display.py'])
+subprocess.Popen(["python", 'parcel-tracker/#display.py'])
 sleep (y)
