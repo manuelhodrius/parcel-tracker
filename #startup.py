8cd1bd20c70b75a7a750de715b78f624ff1a19f5
Manuel Hodrius <manuel@hodrius.com>
2018-02-07 10:21:56 -0800

    update

--- #startup.py
+++ #startup.py
@@ -2,10 +2,10 @@ import subprocess
 from time import sleep
 
 y=(0.1)
-subprocess.Popen(["python3", '#logger.py'])
+subprocess.Popen(["python3", '/home/pi/shipping-logger/#logger.py'])
 sleep(y)
-subprocess.Popen(["python", '#display.py'])
+subprocess.Popen(["python", '/home/pi/shipping-logger/#display.py'])
 sleep (y)
-subprocess.Popen(["python", '#climate.py'])
+subprocess.Popen(["python", '/home/pi/shipping-logger/#climate.py'])
 sleep (y)
 
