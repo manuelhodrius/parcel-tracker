66000e44591ad3db3e3da4ffa6a1e5c56d445d86
Manuel Hodrius <35713351+manuelhodrius@users.noreply.github.com>
2018-02-26 11:01:45 -0800

    added functionality for capacitive senor
    Based on https://bitbucket.org/boblemarin/raspberrypi-capacitive-sensor/src/1c18fad88ae70ce1d83dee9c43528e27664a150d/CapSense1/CapSenseAndLEDs.py?at=master&fileviewer=file-view-default
    which is again based on http://playground.arduino.cc//Main/CapacitiveSensor

---
+++ cap.py
@@ -0,0 +1,55 @@
+#!/user/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+timeout = 10000
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+def CapRead(inPin,outPin,cycles=1,avg=500):
+    # while receive pin is LOW AND total is positive value
+    count = 0
+
+    for j in xrange(0,cycles):
+        sum = 0
+
+        # for loop for average
+        for a in xrange(0,avg):
+
+            # set Send Pin Register low
+            GPIO.setup(outPin, GPIO.OUT)
+            GPIO.output(outPin, GPIO.LOW)
+
+            # set receivePin Register low to make sure pullups are off
+            GPIO.setup(inPin, GPIO.OUT)
+            GPIO.output(inPin, GPIO.LOW)
+            GPIO.setup(inPin, GPIO.IN)
+
+            # set send Pin High
+            GPIO.output(outPin, GPIO.HIGH)
+
+            #while( GPIO.input(inPin) == GPIO.LOW and total < timeout ):
+            while( GPIO.input(inPin) != GPIO.HIGH ):
+                sum = sum + 1
+
+            # set receive pin HIGH briefly to charge up fully - because the while loop above will exit when pin is ~ 2.5V
+            GPIO.setup( inPin, GPIO.OUT )
+            GPIO.output( inPin, GPIO.HIGH )
+            GPIO.setup( inPin, GPIO.IN )
+
+        count = count + round((float(sum)/avg),2)
+
+    return count
+
+    #time.sleep(2/1000)
+
+    # clean before you leave
+    GPIO.cleanup()
+
+# loop
+while True:
+    cyc = CapRead(18,17);
+    print (cyc)
+    #time.sleep(100/1000)
