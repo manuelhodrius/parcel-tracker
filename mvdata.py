cfef1c34e6f6e7c98a4f8779e039bd6443c481c5
Manuel Hodrius <35713351+manuelhodrius@users.noreply.github.com>
2018-02-28 11:39:24 -0800

    rename loggerdata folder rather than all files

--- mvdata.py
+++ mvdata.py
@@ -1,46 +1,20 @@
-import RPi.GPIO as GPIO
-
 import os
 import shutil
 
-# LED
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-
 # set source and destination paths
-source = "/loggerdata/"
-new_folder = "old_data"
-path = source + new_folder
+foldername = "loggerdata"
+path = "/" + foldername
+
+# new folder name
+foldername_new = "o_" + foldername
+path_new = "/" + foldername_new
 
 if os.path.exists(path):
-    print("folder exists")
-else:
-    os.makedirs(path)
-    print ("folder created")
+    # see if it already exists and rename the new folder
+    while os.path.exists(path_new):
+        foldername_new = "o_" + foldername_new
+        path_new = "/" + foldername_new
 
-files = os.listdir(source)
+    os.rename(path, path_new)
 
-for f in files:
-    GPIO.output(18,GPIO.HIGH)
-    # split filename into root and ext
-    file = os.path.splitext(f)
-    root = file[0]
-    ext = file[1]
-
-    # test if the file exists
-    while os.path.exists((source + new_folder + "/" + root + ext)):
-        #print("rename")
-        org = source + root + ext
-        root = "o_" + root
-        new = source + root + ext
-        os.rename(org, new)
-
-    f_from = source + root + ext
-    f_to = source + new_folder + "/" + root + ext
-
-    if not new_folder in f:
-        shutil.move(f_from, f_to)
-    GPIO.output(18,GPIO.LOW)
-
-print("old files moved")
+print("old folder renamed")
