c9e4e7e4713ad054dd90d1578ade1afc22daec0b
Manuel Hodrius <manuel@hodrius.com>
2018-03-20 18:23:02 -0700

    startup

--- #startup.py
+++ #startup.py
@@ -4,8 +4,9 @@ from time import sleep
 #exec("home/pi/shipping-logger/mvdata.py")
 exec(open("/home/pi/shipping-logger/mvdata.py").read())
 
-exec(open("/home/pi/shipping-logger/calibrate.py").read())
+#exec(open("/home/pi/shipping-logger/calibrate.py").read())
 
+'''
 y=(0.1)
 subprocess.Popen(["python3", '/home/pi/shipping-logger/#logger_active.py'])
 sleep(y)
@@ -13,4 +14,4 @@ subprocess.Popen(["python", '/home/pi/shipping-logger/#display.py'])
 sleep (y)
 subprocess.Popen(["python", '/home/pi/shipping-logger/#climate.py'])
 sleep (y)
-
+'''
